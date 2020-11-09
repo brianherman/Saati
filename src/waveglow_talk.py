12107f3372f7aadee16788ac32199f5a456519ee
Joshua Herman <30265+zitterbewegung@users.noreply.github.com>
2020-11-08 20:29:00 -0600

    Text to speech using waveglow from nvidia.

---
+++ src/waveglow_talk.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+from scipy.io.wavfile import write
+
+
+def talk(input: str):
+    waveglow = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_waveglow')
+    waveglow = waveglow.remove_weightnorm(waveglow)
+    waveglow = waveglow.to('cuda')
+    waveglow.eval()
+    tacotron2 = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_tacotron2')
+    tacotron2 = tacotron2.to('cuda')
+    tacotron2.eval()
+    # preprocessing
+    sequence = np.array(tacotron2.text_to_sequence(text, ['english_cleaners']))[None, :]
+    sequence = torch.from_numpy(sequence).to(device='cuda', dtype=torch.int64)
+
+    # run the models
+    with torch.no_grad():
+    _, mel, _, _ = tacotron2.infer(sequence)
+    audio = waveglow.infer(mel)
+    audio_numpy = audio[0].data.cpu().numpy()
+    rate = 22050
+    write("audio.wav", rate, audio_numpy)
+    return audio
